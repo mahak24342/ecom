03fe3a02fb46a310fcaeaf9d4ad5a08d7bd10645
mahak porwal <you@mahak.com>
2023-05-02 23:53:43 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assests/1.png";
+import product2 from "./assests/2.png";
+import product3 from "./assests/33.png";
+import product4 from "./assests/4.png";
+import product5 from "./assests/5.png";
+import product6 from "./assests/6.webp";
+import product7 from "./assests/7.webp";
+import product8 from "./assests/8.webp";
+export const PRODUCTS=[
+    {
+        id:1,
+        productName:"IPHONE",
+        price:999.0,
+        productImage:product1,
+    },
+
+ {   id:2,
+    productName:"GOLD",
+    price:999.0,
+    productImage:product2,
+ },
+
+
+ {
+    id:3,
+ productName:"WATCH",
+ price:999.0,
+ productImage:product3,
+},
+
+{   id:4,
+    productName:"GOLD",
+    price:999.0,
+    productImage:product4,
+ },
+ 
+ {   id:5,
+    productName:"GOLD",
+    price:999.0,
+    productImage:product5,
+ },
+ 
+ {   id:6,
+    productName:"GOLD",
+    price:999.0,
+    productImage:product6,
+ },
+ 
+ {   id:7,
+    productName:"EARPODS",
+    price:999.0,
+    productImage:product7,
+ },
+ 
+ {   id:8,
+    productName:"GOLDFISH",
+    price:999.0,
+    productImage:product8,
+ }
+]
